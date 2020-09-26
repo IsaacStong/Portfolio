9ef72d604b3a724f0c05794e3f5a14fe05f50301
IsaacStong <isaacstong90@gmail.com>
2020-09-26 18:17:52 -0400

    Soduku
    Added Sudoku App.
    Working on creating a working sudoku app using JS, HTML, and CSS

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', include('webpage.urls')),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('sudoku/', views.sudoku, name='sudoku'),
 ]
--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,1 +1,8 @@
+from django.shortcuts import render
 
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def sudoku(request):
+    return render(request, 'Sudoku.html')
