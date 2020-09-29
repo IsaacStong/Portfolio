05ecbb7a115ddedbee427886f53d6ad9c6e327a9
IsaacStong <isaacstong90@gmail.com>
2020-09-29 16:33:32 -0400

    Board Update
    Added board and a board population in JS.
    Made it so default values or uneditable and gray for easier visuals.

--- static/Portfolio/JS/Sudoku.js
+++ static/Portfolio/JS/Sudoku.js
@@ -1,22 +1,27 @@
+// game boards for Sudoku.
+const board1 = [
+  ['5', '3', '', '', '7', '', '', '', ''],
+  ['6', '', '', '1', '9', '5', '', '', ''],
+  ['', '9', '8', '', '', '', '', '6', ''],
+  ['8', '', '', '', '6', '', '', '', '3'],
+  ['4', '', '', '8', '', '3', '', '', '1'],
+  ['7', '', '', '', '2', '', '', '', '6'],
+  ['', '6', '', '', '', '', '2', '8', ''],
+  ['', '', '', '4', '1', '9', '', '', '5'],
+  ['', '', '', '', '8', '', '', '7', '9']
+];
 
-
-document.querySelector('.header__select--wrapper').addEventListener('click', function() {
-    this.querySelector('.header__select').classList.toggle('open');
-})
-
-for (const option of document.querySelectorAll("header__select--option")) {
-    option.addEventListener('click', function() {
-        if (!this.classList.contains('selected')) {
-            this.parentNode.querySelector('.header__select--option.selected').classList.remove('selected');
-            this.classList.add('selected');
-            this.closest('.header__select').querySelector('.header__select--trigger span').textContent = this.textContent;
-        }
-    })
-}
-
-window.addEventListener('click', function(e) {
-    const select = document.querySelector('.header__select')
-    if (!select.contains(e.target)) {
-        select.classList.remove('open');
+// Select all squares from HTML and populate using board.
+blocks = document.querySelectorAll('input');
+var k = 0;
+for(var i = 0; i < 9; ++i) {
+  for(var j = 0; j < 9; ++j) {
+    blocks[k].value = board1[j][i];
+    //if statement makes given Sudoku values gray and uneditable.
+    if (blocks[k].value != '') {
+      blocks[k].readOnly = true;
+      blocks[k].style.color = 'gray';
     }
-});
+    k = k+1;
+  }
+}
